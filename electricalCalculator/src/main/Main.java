4fa162423d8b6762c4e775de4cbecef5c0e0d216
ignaciofp60 <ignaciofp60@educastur.es>
2022-12-28 00:53:05 +0100

    Added scanDir() and testing clearConsole()

--- electricalCalculator/src/main/Main.java
+++ electricalCalculator/src/main/Main.java
@@ -2,51 +2,54 @@ package main;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-
-import pearls.Help;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Hello! Welcome to this shitty program!\nDo you need some help?\nYou have these options:\n");
+//		System.out.println("Hello! Welcome to this shitty program!\nDo you need some help?\nYou have these options:\n");
+
+		String[] pearls = scanDir();
+
+		for (int i = 0; i < pearls.length; i++)
+			System.out.println((i + 1) + ": " + pearls[i]);
+
+		int optionSelected = 0;
+
+		System.out.print("\nChoose an option: ");
+
+		while ((optionSelected = Integer.parseInt(in.readLine())) > pearls.length || optionSelected == 0) {
+			if (optionSelected > pearls.length || optionSelected == 0)
+				System.out.print("\nWe didn't implemented that option yet, sorry. :(\nTry again: ");
+		}
 		
-		System.out.println(Arrays.toString(scanDir()));
-		
-		
-//	    inp1 = 0
-//	    while inp1 <= 0:
-//	        try:
-//	            inp1 = int(input('\nYour choice: '))
-//	            lenop = len(pyfiles)
-//	            if inp1 > lenop:
-//	                print('I\'m sorry, that option isn\'t ready yet')
-//	                timer_5s()
-//
-//	        except:
-//	            print('Sorry, what you said?')
-//	            inp1 = 0
+		clearConsole();
+
+		System.out.println("Your selection: " + optionSelected);
+		System.out.println("\nLaunching " + pearls[optionSelected - 1] + "...");
 
 	}
-	
+
 	public static String[] scanDir() {
-		String sCarpAct = System.getProperty(Main.class.getResourceAsStream("/pearls"));
-		File carpeta = new File(sCarpAct);
-		
+		File carpeta = new File("resources/pearls");
 		String[] listado = carpeta.list();
 		if (listado == null || listado.length == 0) {
-		    System.out.println("No hay elementos dentro de la carpeta actual");
-		    return null;
+			System.out.println("No hay elementos dentro de la carpeta actual");
+			return null;
 		}
-		else {
-		    for (int i=0; i< listado.length; i++) {
-		        System.out.println(listado[i]);
-		    }
-		}
-		
 		return listado;
+	}
+
+	public static void clearConsole() {
+		//Clears Screen in java
+	    try {
+	        if (System.getProperty("os.name").contains("Windows"))
+	        	new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+	        else
+	            Runtime.getRuntime().exec("clear");
+	    } catch (IOException | InterruptedException ex) {}
 	}
 
 }
