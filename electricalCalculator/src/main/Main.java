38743f3020ad24a3be11e11856d42c01d07a4d61
ignaciofp60 <ignaciofp60@educastur.es>
2022-12-27 22:02:54 +0100

    first commit

---
+++ electricalCalculator/src/main/Main.java
@@ -0,0 +1,26 @@
+package main;
+
+import pearls.Help;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello! Welcome to this shitty program!\nDo you need some help?\nYou have these options:\n");
+		
+		system.print_path()
+	    inp1 = 0
+	    while inp1 <= 0:
+	        try:
+	            inp1 = int(input('\nYour choice: '))
+	            lenop = len(pyfiles)
+	            if inp1 > lenop:
+	                print('I\'m sorry, that option isn\'t ready yet')
+	                timer_5s()
+
+	        except:
+	            print('Sorry, what you said?')
+	            inp1 = 0
+
+	}
+
+}
---
+++ electricalCalculator/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Nacho
+ *
+ */
+module electricalCalculator {
+}
