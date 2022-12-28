a91fae837e3b238e75b58e9ef2a7b643d0fd48a8
nafdez <fzp.nacho@gmail.com>
2022-12-28 16:21:39 +0100

    ckpoint

---
+++ electricalCalculator/resources/scripts/Help.java
@@ -0,0 +1,12 @@
+package scripts;
+
+public class Help {	
+	public Help() {
+	}	
+	
+	public static void printHelp() {
+		System.out.println("Hi, to add extra calculator funcionalities, you only need to download or program your own scripts!.");
+		System.out.println("Once you have done this, only you need to do is to paste the file \'.java\' on the package at \'/resources/scripts\'.");
+		System.out.println("Thanks for using this program!");
+	}
+}
--- electricalCalculator/src/main/Main.java
+++ electricalCalculator/src/main/Main.java
@@ -5,35 +5,45 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import scripts.Help;
+
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-//		System.out.println("Hello! Welcome to this shitty program!\nDo you need some help?\nYou have these options:\n");
+		System.out.println("Hello! Welcome to this shitty program!\nDo you need some help?\nYou have these options:\n");
 
-		String[] pearls = scanDir();
+		String[] scripts = scanDir();
 
-		for (int i = 0; i < pearls.length; i++)
-			System.out.println((i + 1) + ": " + pearls[i]);
+		for (int i = 0; i < scripts.length; i++)
+			System.out.println((i + 1) + ": " + scripts[i]);
 
 		int optionSelected = 0;
 
 		System.out.print("\nChoose an option: ");
 
-		while ((optionSelected = Integer.parseInt(in.readLine())) > pearls.length || optionSelected == 0) {
-			if (optionSelected > pearls.length || optionSelected == 0)
+		while ((optionSelected = Integer.parseInt(in.readLine())) > scripts.length || optionSelected == 0) {
+			if (optionSelected > scripts.length || optionSelected == 0)
 				System.out.print("\nWe didn't implemented that option yet, sorry. :(\nTry again: ");
 		}
 		
 		clearConsole();
 
 		System.out.println("Your selection: " + optionSelected);
-		System.out.println("\nLaunching " + pearls[optionSelected - 1] + "...");
+		System.out.println("\nLaunching " + scripts[optionSelected - 1] + "...");
+		
+		openFile(scripts[optionSelected - 1]);
 
+	}
+	
+	public static void openFile(String file) {
+		File script = new File("resources/scripts/" + file);
+		System.out.println(script);
+		
 	}
 
 	public static String[] scanDir() {
-		File carpeta = new File("resources/pearls");
+		File carpeta = new File("resources/scripts");
 		String[] listado = carpeta.list();
 		if (listado == null || listado.length == 0) {
 			System.out.println("No hay elementos dentro de la carpeta actual");
