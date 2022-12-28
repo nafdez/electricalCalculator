67a3b97d080e815385f4326a6f23867751c04fd7
ignaciofp60 <ignaciofp60@educastur.es>
2022-12-28 19:44:21 +0100

    zz

--- electricalCalculator/src/main/Main.java
+++ electricalCalculator/src/main/Main.java
@@ -4,15 +4,13 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-import scripts.Help;
+import java.lang.ProcessBuilder.Redirect;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Hello! Welcome to this shitty program!\nDo you need some help?\nYou have these options:\n");
-
 		String[] scripts = scanDir();
 
 		for (int i = 0; i < scripts.length; i++)
@@ -26,20 +24,47 @@ public class Main {
 			if (optionSelected > scripts.length || optionSelected == 0)
 				System.out.print("\nWe didn't implemented that option yet, sorry. :(\nTry again: ");
 		}
-		
+
 		clearConsole();
 
 		System.out.println("Your selection: " + optionSelected);
 		System.out.println("\nLaunching " + scripts[optionSelected - 1] + "...");
-		
-		openFile(scripts[optionSelected - 1]);
+
+		openFile(scripts[optionSelected - 1], in);
 
 	}
-	
-	public static void openFile(String file) {
-		File script = new File("resources/scripts/" + file);
-		System.out.println(script);
-		
+
+	public static void openFile(String file, BufferedReader in) throws IOException {
+		clearConsole();
+		System.out.println("\nLaunching " + file + "...");
+		String x = "y";
+		while (x.toLowerCase() == "y") {
+//			System.out.println("java " + getResPath() + "\\scripts." + file.replaceAll(".class", ""));
+//			Runtime.getRuntime().exec("java " + getResPath() + "\\scripts." + file.replaceAll(".class", ""));
+			String[] cmd = {"java", "-cp", getResPath() + "\\scripts.", file.replaceAll(".class", "")};
+			Process p = new ProcessBuilder(cmd).redirectError(Redirect.INHERIT)
+			                                   .redirectOutput(Redirect.INHERIT)
+			                                   .start();
+			try {
+				p.waitFor();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("\nDo you want to continue using this script? (y/N)");
+			x = in.readLine();
+			
+			if (x.toLowerCase() == "y") {
+				clearConsole();
+				System.out.println("Reloading" + file + "...\n");
+			}
+		}
+		System.out.println("Exiting the script.");
+	}
+
+	public static String getResPath() {
+		File f = new File("resources/");
+		return f.getAbsolutePath();
 	}
 
 	public static String[] scanDir() {
@@ -53,13 +78,14 @@ public class Main {
 	}
 
 	public static void clearConsole() {
-		//Clears Screen in java
-	    try {
-	        if (System.getProperty("os.name").contains("Windows"))
-	        	new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-	        else
-	            Runtime.getRuntime().exec("clear");
-	    } catch (IOException | InterruptedException ex) {}
+		// Clears Screen in java
+		try {
+			if (System.getProperty("os.name").contains("Windows"))
+				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+			else
+				Runtime.getRuntime().exec("clear");
+		} catch (IOException | InterruptedException ex) {
+		}
 	}
 
 }
--- electricalCalculator/src/scriptsSrc/Help.java
+++ electricalCalculator/src/scriptsSrc/Help.java
@@ -1,10 +1,7 @@
-package scripts;
+package scriptsSrc;
 
 public class Help {	
-	public Help() {
-	}	
-	
-	public static void printHelp() {
+	public static void main(String[] args) {
 		System.out.println("Hi, to add extra calculator funcionalities, you only need to download or program your own scripts!.");
 		System.out.println("Once you have done this, only you need to do is to paste the file \'.java\' on the package at \'/resources/scripts\'.");
 		System.out.println("Thanks for using this program!");
