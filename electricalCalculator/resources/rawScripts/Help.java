dbf4c10f2d52ea87bc3498ed1fd3d5e2e51c9dfd
ignaciofp60 <ignaciofp60@educastur.es>
2022-12-29 17:57:07 +0100

    wef

--- electricalCalculator/resources/rawScripts/Help.java
+++ electricalCalculator/resources/rawScripts/Help.java
@@ -3,7 +3,7 @@ package rawScripts;
 public class Help {	
 	public static void main(String[] args) {
 		System.out.println("Hi, to add extra calculator funcionalities, you only need to download or program your own scripts!.");
-		System.out.println("Once you have done this, only you need to do is to paste the file \'.class\' on the package at \'/resources/scripts\'.");
+		System.out.println("Once you have done this, only you need to do is to paste the file \'.class\' on the package at \'/scripts\'.");
 		System.out.println("Thanks for using this program!");
 	}
 }
