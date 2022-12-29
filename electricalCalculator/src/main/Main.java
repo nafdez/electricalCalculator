de449cfc20159dafa1fa617703d8587c7bc7cb3c
ignaciofp60 <ignaciofp60@educastur.es>
2022-12-29 01:55:07 +0100

    working 1/2

--- electricalCalculator/src/main/Main.java
+++ electricalCalculator/src/main/Main.java
@@ -9,6 +9,7 @@ import java.lang.ProcessBuilder.Redirect;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
+		clearConsole();
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Hello! Welcome to this shitty program!\nDo you need some help?\nYou have these options:\n");
 		String[] scripts = scanDir();
@@ -28,7 +29,6 @@ public class Main {
 		clearConsole();
 
 		System.out.println("Your selection: " + optionSelected);
-		System.out.println("\nLaunching " + scripts[optionSelected - 1] + "...");
 
 		openFile(scripts[optionSelected - 1], in);
 
@@ -36,32 +36,27 @@ public class Main {
 
 	public static void openFile(String file, BufferedReader in) throws IOException {
 		clearConsole();
-		System.out.println("\nLaunching " + file + "...");
+		System.out.print("\nLaunching " + file);
+		
+		laTonteriaDeLosPuntitos(".", 3);
+		clearConsole();
+		
 		String x = "y";
 		while (x.toLowerCase() == "y") {
-//			System.out.println("java " + getResPath() + "\\scripts." + file.replaceAll(".class", ""));
-//			Runtime.getRuntime().exec("java " + getResPath() + "\\scripts." + file.replaceAll(".class", ""));
-			String[] cmd = {"java", "-cp", getResPath() + "\\scripts.", file.replaceAll(".class", "")};
-			Process p = new ProcessBuilder(cmd).redirectError(Redirect.INHERIT)
-			                                   .redirectOutput(Redirect.INHERIT)
+			String[] cmd = {"java", "-cp", getResPath(), "scripts." + file.replaceAll(".class", "")};
+			Process p = new ProcessBuilder(cmd).redirectError(Redirect.INHERIT) // No se muy bien lo que hace, lo saqué de internet
+			                                   .redirectOutput(Redirect.INHERIT) // pero con esto se puede abrir una clase usando la terminal
 			                                   .start();
-//			String classLoad = "scripts.Help";
-//			try {
-//				Class.forName(classLoad);
-//			} catch (ClassNotFoundException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
+			System.out.println();
 			try {
 				p.waitFor();
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			System.out.println("\nDo you want to continue using this script? (y/N)");
+			System.out.print("\nDo you want to continue using this script? (y/N): ");
 			x = in.readLine();
 			
-			if (x.toLowerCase() == "y") {
+			if (x.toLowerCase() == "y") { // Not working the moment
 				clearConsole();
 				System.out.println("Reloading" + file + "...\n");
 			}
@@ -70,7 +65,7 @@ public class Main {
 	}
 
 	public static String getResPath() {
-		File f = new File("scripts");
+		File f = new File("");
 		return f.getAbsolutePath();
 	}
 
@@ -83,14 +78,28 @@ public class Main {
 		}
 		return listado;
 	}
+	
+	public static void laTonteriaDeLosPuntitos(String msj, int duration) {
+		for(int i = 0; i < 3; i++) {
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.print(".");
+		}
+		// Pequeño método que en una línea repite el caracter o cadena que queramos y el número de veces
+		// No cumple ninguna función, pero en Python lo había hecho así y me salió de ahí meterlo aquí
+	}
 
 	public static void clearConsole() {
 		// Clears Screen in java
+		String[] cmd = {"clear"};
 		try {
 			if (System.getProperty("os.name").contains("Windows"))
 				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
 			else
-				Runtime.getRuntime().exec("clear");
+				Runtime.getRuntime().exec(cmd);
 		} catch (IOException | InterruptedException ex) {
 		}
 	}
