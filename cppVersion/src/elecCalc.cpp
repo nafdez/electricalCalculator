2aab146b5a8ad11e48d722cd6f4a0d7dee2921a2
ignaciofp60 <ignaciofp60@educastur.es>
2023-01-06 19:21:58 +0100

    r

--- cppVersion/src/elecCalc.cpp
+++ cppVersion/src/elecCalc.cpp
@@ -13,38 +13,42 @@ namespace fs = std::filesystem;
 // On the other hand, the for loop read the name of each file and prints to console
 // Pendiente de cambios:
 // Cambiar src a la carpeta final en la que estará, o en su defecto camiar el regex_replace por un concat 'scripts'.
-string scriptSelector(){
-    string path = fs::current_path();
+string scriptSelector()
+{
+    string path = fs::current_path().string();
     path = regex_replace(path, regex("src"), "scripts");
 
     int menuNumber = 0;
     vector<string> files;
 
-    for (const auto & file : fs::directory_iterator(path)) {
-        files.push_back (file.path());
+    for (const auto &file : fs::directory_iterator(path))
+    {
+        files.push_back(file.path().string());
         string fileWithoutPath = regex_replace(files.at(menuNumber), regex(path + "/"), "");
-        cout << ++menuNumber << ". " <<  fileWithoutPath << endl;
+        cout << ++menuNumber << ". " << fileWithoutPath << endl;
     }
 
     cout << "\nWhich program do you want to run?" << endl;
 
     int option;
-    
+
     cin >> option;
     option = option - 1;
 
     return regex_replace(files.at(option), regex(path + "/"), "");
 }
 
-void scriptLauncher(string filePath){
+void scriptLauncher(string filePath)
+{
     filePath = filePath.insert(0, "./");
     system(("cd ../scripts && ./" + filePath).c_str());
     cout << "hola";
 }
 
-int main(){
-    cout << "Hello! Welcome to this shitty program!\nDo you need some help?\nYou have these options:\n" << endl;
+int main()
+{
+    cout << "Hello! Welcome to this shitty program!\nDo you need some help?\nYou have these options:\n"
+         << endl;
     scriptLauncher(scriptSelector());
     return 0;
 }
-
