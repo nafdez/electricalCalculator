79aaece9f216684ac61a0405a4285d9e23faaa4d
ignaciofp60 <ignaciofp60@educastur.es>
2023-01-06 04:07:09 +0100

    test

--- cppVersion/src/elecCalc.cpp
+++ cppVersion/src/elecCalc.cpp
@@ -4,32 +4,47 @@
 #include <string>
 #include <filesystem>
 #include <regex>
+#include <cstdlib>
 
 using namespace std;
 namespace fs = std::filesystem;
-
-void getDirScanDir(); // for some reason I have to declare before main too
-
-int main(){
-    getDirScanDir();
-    return 0;
-}
 
 // The code below first scans the execution dir and then, using regex changes from the src to the scripts folder.
 // On the other hand, the for loop read the name of each file and prints to console
 // Pendiente de cambios:
 // Cambiar src a la carpeta final en la que estará, o en su defecto camiar el regex_replace por un concat 'scripts'.
-void getDirScanDir(){
-    cout << "Current path is " << fs::current_path() << '\n'; // (1)
-    
+string scriptSelector(){
     string path = fs::current_path();
     path = regex_replace(path, regex("src"), "scripts");
 
     int menuNumber = 0;
+    vector<string> files;
 
     for (const auto & file : fs::directory_iterator(path)) {
-        string f = file.path();
-        f = regex_replace(f, regex(path + "/"), "");
-        cout << ++menuNumber << ". " <<  f << endl;
+        files.push_back (file.path());
+        string fileWithoutPath = regex_replace(files.at(menuNumber), regex(path + "/"), "");
+        cout << ++menuNumber << ". " <<  fileWithoutPath << endl;
     }
+
+    cout << "\nWhich program do you want to run?" << endl;
+
+    int option;
+    
+    cin >> option;
+    option = option - 1;
+
+    return regex_replace(files.at(option), regex(path + "/"), "");
 }
+
+void scriptLauncher(string filePath){
+    filePath = filePath.insert(0, "./");
+    system(("cd ../scripts && ./" + filePath).c_str());
+    cout << "hola";
+}
+
+int main(){
+    cout << "Hello! Welcome to this shitty program!\nDo you need some help?\nYou have these options:\n" << endl;
+    scriptLauncher(scriptSelector());
+    return 0;
+}
+
